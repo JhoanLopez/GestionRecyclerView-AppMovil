5e4d7a78ede8963de4e37dc297a3a6a970c52cb3
Jhoan López <jhoanlopezclase@gmail.com>
2022-02-13 09:01:40 +0100

    Versión 1.0

--- app/src/main/java/com/jhoanlopez/gestiondecontactos/MainActivity.java
+++ app/src/main/java/com/jhoanlopez/gestiondecontactos/MainActivity.java
@@ -12,6 +12,7 @@ import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.nio.charset.StandardCharsets;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,15 +28,21 @@ public class MainActivity extends AppCompatActivity {
 
         categoriaElegida = findViewById(R.id.tv_categoriaElegida);
 
-        //Así se extraen los datos después de hacer un putExtra en otra pantalla
+        //Así se extraen los categoria después de hacer un putExtra en otra pantalla
         Intent recibir = getIntent();
-        String datos = recibir.getStringExtra("1");
+        String categoria = recibir.getStringExtra("1");
 
-        categoriaElegida.setText(datos);
-        init();
+        if (categoria.equalsIgnoreCase("Play Station Gamers")) {
+            addPlayStationStreamers();
+        } else if (categoria.equalsIgnoreCase("XBOX Gamers")) {
+            addXboxStreamers();
+        } else if (categoria.equalsIgnoreCase("PC Gamers")) {
+            addPcStreamers();
+        }
+        categoriaElegida.setText(categoria);
     }
 
-    public void init() {
+    public void addPlayStationStreamers() {
         elementos = new ArrayList<>();
         elementos.add(new ListElement("#29B6F6", "Pedro", "Mexico", "Activo"));
         elementos.add(new ListElement("#CB4335", "Juan", "España", "Activo"));
@@ -58,7 +65,56 @@ public class MainActivity extends AppCompatActivity {
         recyclerView.setHasFixedSize(true);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         recyclerView.setAdapter(listAdapter);
+    }
 
+    public void addXboxStreamers() {
+        elementos = new ArrayList<>();
+        elementos.add(new ListElement("#512DA8", "Diego Franco", "Mexico", "Activo"));
+        elementos.add(new ListElement("#CB4335", "Markos ", "España", "Activo"));
+        elementos.add(new ListElement("#6C3483", "Arnold", "USA", "Activo"));
+        elementos.add(new ListElement("#E64A19", "Felipe", "Brasil", "Activo"));
+        elementos.add(new ListElement("#AD1457", "Mateo", "Argentina", "Activo"));
+        elementos.add(new ListElement("#7CB342", "Franccesco", "Italia", "Activo"));
+        elementos.add(new ListElement("#FFEB3B", "Camilo", "Colombia", "Activo"));
+        elementos.add(new ListElement("#0D47A1", "Tyrone", "Venezuela", "Activo"));
+        elementos.add(new ListElement("#37474F", "Drake", "Canadá", "Activo"));
+
+
+        ListAdapter listAdapter = new ListAdapter(elementos, this, new ListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(ListElement item) {
+                moveToDescripcion(item);
+            }
+        });
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
+    }
+
+    public void addPcStreamers() {
+        elementos = new ArrayList<>();
+        elementos.add(new ListElement("#512DA8", "El Rubius", "España", "Activo"));
+        elementos.add(new ListElement("#CB4335", "Juan", "España", "Activo"));
+        elementos.add(new ListElement("#512DA8", "Ibai Llanos", "España", "Activo"));
+        elementos.add(new ListElement("#E64A19", "Felipe", "Brasil", "Activo"));
+        elementos.add(new ListElement("#AD1457", "Mateo", "Argentina", "Activo"));
+        elementos.add(new ListElement("#7CB342", "Franccesco", "Italia", "Activo"));
+        elementos.add(new ListElement("#FFEB3B", "Camilo", "Colombia", "Activo"));
+        elementos.add(new ListElement("#512DA8", "Kun Agüero", "Argentina", "Activo"));
+        elementos.add(new ListElement("#512DA8", "Auron Play", "España", "Activo"));
+
+
+        ListAdapter listAdapter = new ListAdapter(elementos, this, new ListAdapter.OnItemClickListener() {
+            @Override
+            public void onItemClick(ListElement item) {
+                moveToDescripcion(item);
+            }
+        });
+        RecyclerView recyclerView = findViewById(R.id.recycler);
+        recyclerView.setHasFixedSize(true);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setAdapter(listAdapter);
     }
 
 
